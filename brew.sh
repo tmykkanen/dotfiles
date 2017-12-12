74cf833006bbb5c9fa57a0c14c08e1824692456d
tmykkanen <mr.tyler.mykkanen@gmail.com>
2017-12-11 21:45:47 -0600

    added mongodb and sudo loop

--- brew.sh
+++ brew.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `brew.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
@@ -71,6 +77,12 @@ brew install vbindiff
 brew install ffmpeg
 brew install hugo
 brew install lame
+
+# Development tools
+brew install mongodb
+sudo mkdir -p /data/db
+sudo chmod 777 /data/db
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
